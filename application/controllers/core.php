25e8bc7e8e6fb4c2ad8aad7894742cc4235cd057
WhisnuSD <whisnu@shopdeca.com>
2013-11-08 16:14:05 +0700

    second commit

---
+++ application/controllers/core.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Core extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct() {
+            parent::__construct();
+            //$this->config->set_item('compress_output', TRUE); 
+            $this->load->helper(array('url'));
+            $this->load->library(array('session','general','email'));
+            
+			}
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
